ce99116d8783d2713ede900bfff9b2c2a234ee79
Jose Vazquez <vazqjose@gmail.com>
2021-07-22 05:04:21 +0000

    t14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,23 +8,18 @@
  * Return: If tree is NULL, return 0
  */
 
- int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int lHeight;
 	int rHeight;
 
 	if (tree == NULL)
 		return (0);
-	
+
 	lHeight = binary_tree_balance(tree->left) + 1;
 	rHeight = binary_tree_balance(tree->right) + 1;
 
 	return (abs(rHeight - lHeight));
-	/*
-	if (abs(rHeight - lHeight) > 0 && binary_tree_balance(tree->left)
-			&& binary_tree_balance(tree->right))
-		return (abs(rHeight - lHeight));
-	*/
-	return (0);
 
+	return (0);
 }
